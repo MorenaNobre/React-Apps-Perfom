172a4c8ae505d459832838ec9e47e6f9b86c66f2
MorenaNobre <morenagnobre@gmail.com>
2022-08-23 11:51:13 -0300

    useMemo

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
@@ -5,17 +6,22 @@ interface SearchResultsProps {
     id: number;
     price: number;
     title: string;
-  }>
+  }>;
 }
 
-export function SearchResults({results}: SearchResultsProps) {
+export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price;
+    }, 0);
+  }, [results]);
+
   return (
     <div>
-      {results.map(product => {
-        return (
-          <ProductItem product={product}/>
-        )
+      <h2>{totalPrice}</h2>
+      {results.map((product) => {
+        return <ProductItem product={product} />;
       })}
     </div>
-  )
+  );
 }
